266fa5f746497336f4617717b2530bc4b4b90c19
Gerard <g.roma@hud.ac.uk>
2019-02-19 14:53:32 +0000

    add FluidOnsetSlice

---
+++ src/FluidOnsetSlice/FluidOnsetSlice.cpp
@@ -0,0 +1,13 @@
+
+// A tool from the FluCoMa project, funded by the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (grant agreement No 725899)
+
+#include <clients/rt/OnsetSlice.hpp>
+#include <FluidSCWrapper.hpp>
+
+static InterfaceTable *ft;
+
+PluginLoad(FluidSTFTUGen) {
+  ft = inTable;
+  using namespace fluid::client;
+  makeSCWrapper<OnsetSlice<double,float>>(ft, "FluidOnsetSlice");
+}
