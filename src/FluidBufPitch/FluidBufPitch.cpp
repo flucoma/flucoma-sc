37751b630731c3198b350c1a89db9b5f4c69e660
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2019-04-25 16:51:54 +0100

    bufPitch and bufMFCC correct name to the table

--- src/FluidBufPitch/FluidBufPitch.cpp
+++ src/FluidBufPitch/FluidBufPitch.cpp
@@ -9,5 +9,5 @@ static InterfaceTable *ft;
 PluginLoad(FluidSTFTUGen) {
   ft = inTable;
   using namespace fluid::client;
-  makeSCWrapper<NRTPitchClient>("FluidBufPitch", ft);
+  makeSCWrapper<NRTPitchClient>("BufPitch", ft);
 }
