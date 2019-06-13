bf7ab1680ccab7a231a614fef98338a053272f99
Gerard <g.roma@hud.ac.uk>
2019-06-13 14:57:38 -0400

    Rename RTNoveltySlice to NoveltySlice

--- src/FluidBufNoveltySlice/FluidBufNoveltySlice.cpp
+++ src/FluidBufNoveltySlice/FluidBufNoveltySlice.cpp
@@ -1,15 +1,14 @@
-  // FD_BufNMF, an NRT buffer NMF Processor
+
 // A tool from the FluCoMa project, funded by the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (grant agreement No 725899)
 
-
-#include <clients/nrt/NoveltyClient.hpp>
+#include <clients/rt/NoveltySlice.hpp>
+#include <clients/nrt/FluidNRTClientWrapper.hpp>
 #include <FluidSCWrapper.hpp>
 
-static InterfaceTable* ft; 
+static InterfaceTable *ft;
 
-PluginLoad(OfflineFluidDecompositionUGens)
-{
+PluginLoad(OfflineFluidDecompositionUGens) {
   ft = inTable;
   using namespace fluid::client;
-  makeSCWrapper<NoveltyClient>("BufNoveltySlice", ft);
+  makeSCWrapper<NRTNoveltySlice>("BufNoveltySlice", ft);
 }
