6960c1b95e6c930e3e5359a0f183260d4b9ffa9b
Owen Green <gungwho@gmail.com>
2019-08-07 14:15:11 +0100

    Add message testing SC class and test code

---
+++ src/FluidMessageTest/FluidMessageTest.cpp
@@ -0,0 +1,13 @@
+
+// A tool from the FluCoMa project, funded by the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (grant agreement No 725899)
+
+#include <clients/nrt/FluidMessageTest.hpp>
+#include <FluidSCWrapper.hpp>
+
+static InterfaceTable *ft;
+
+PluginLoad(FluidSTFTUGen) {
+  ft = inTable;
+  using namespace fluid::client;
+  makeSCWrapper<NRTThreadingMessageTest>("FluidMessageTest", ft);
+}
