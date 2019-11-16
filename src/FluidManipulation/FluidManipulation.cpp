26b341f109e8c3cbfa5f42ce56cfb35444943c57
Owen Green <gungwho@gmail.com>
2019-11-16 10:26:24 +0000

    src/FluidManipulation/FluidManipulation.cpp: Update incluedes and object defs

--- src/FluidManipulation/FluidManipulation.cpp
+++ src/FluidManipulation/FluidManipulation.cpp
@@ -1,8 +1,17 @@
 
 // A tool from the FluCoMa project, funded by the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (grant agreement No 725899)
 
-#include <clients/nrt/DatasetClient.hpp>
 #include <clients/nrt/CorpusClient.hpp>
+#include <clients/nrt/DataSetClient.hpp>
+#include <clients/nrt/LabelSetClient.hpp>
+#include <clients/nrt/KDTreeClient.hpp>
+#include <clients/nrt/KMeansClient.hpp>
+#include <clients/nrt/KNNClasClient.hpp>
+#include <clients/nrt/KNNRegClient.hpp>
+#include <clients/nrt/KNNClient.hpp>
+#include <clients/nrt/NormalizeClient.hpp>
+#include <clients/nrt/StandardizeClient.hpp>
+#include <clients/rt/AudioTransportClient.hpp>
 #include <FluidSCWrapper.hpp>
 
 static InterfaceTable *ft;
@@ -11,6 +20,15 @@ PluginLoad(FluidSTFTUGen)
 {
   ft = inTable;
   using namespace fluid::client;
-  makeSCWrapper<NRTThreadedDataset>("FluidDataset", ft);
-  makeSCWrapper<NRTThreadedCorpus>("FluidCorpus", ft);
+  makeSCWrapper<NRTThreadedDataSetClient>("FluidDataSet",ft);
+  makeSCWrapper<NRTThreadedLabelSetClient>("FluidLabelSet",ft);
+  makeSCWrapper<NRTThreadedKDTreeClient>("FluidKDTree",ft);
+  makeSCWrapper<NRTThreadedKMeansClient>("FluidKMeans",ft);
+  makeSCWrapper<NRTThreadedKNNClasClient>("FluidKNNClassifier",ft);
+  makeSCWrapper<NRTThreadedKNNRegClient>("FluidKNNRegressor",ft);
+  makeSCWrapper<NRTThreadedKNNClient>("FluidKNN",ft);
+  makeSCWrapper<NRTThreadedNormalizeClient>("FluidNormalize",ft);
+  makeSCWrapper<NRTThreadedStandardizeClient>("FluidStandardize",ft);
+  makeSCWrapper<RTAudioTransportClient>("FluidAudioTransport",ft);
+  makeSCWrapper<NRTThreadedAudioTransportClient>("FluidBufAudioTransport",ft);
 }
