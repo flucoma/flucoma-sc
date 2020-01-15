9677ad83f679b955e6dd13b6265353d975282cab
Owen Green <gungwho@gmail.com>
2020-01-15 15:42:47 +0000

    Add header text to source files

--- include/SCBufferAdaptor.hpp
+++ include/SCBufferAdaptor.hpp
@@ -1,3 +1,13 @@
+/*
+Part of the Fluid Corpus Manipulation Project (http://www.flucoma.org/)
+Copyright 2017-2019 University of Huddersfield.
+Licensed under the BSD-3 License.
+See license.md file in the project root for full license information.
+This project has received funding from the European Research Council (ERC)
+under the European Union’s Horizon 2020 research and innovation programme
+(grant agreement No 725899).
+*/
+
 #pragma once
 
 #include <SC_PlugIn.h>
@@ -196,4 +206,3 @@ std::ostream& operator <<(std::ostream& os, SCBufferAdaptor& b)
 
 } // namespace client
 } // namespace fluid
-
