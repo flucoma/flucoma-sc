745150e354b47674b1d93c9e9a640af07eecf874
Owen Green <gungwho@gmail.com>
2020-03-23 15:27:34 +0000

    Statically link runtime on Windows

--- scripts/target_post.cmake
+++ scripts/target_post.cmake
@@ -8,6 +8,16 @@
 target_compile_features(${PLUGIN} PRIVATE cxx_std_14)
 
 if(MSVC)
+  foreach(flag_var
+      CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+      CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+    if(${flag_var} MATCHES "/MD")
+      string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+    endif()
+  endforeach()
+endif()
+
+if(MSVC)
   target_compile_options(${PLUGIN} PRIVATE /W3)
 else()
   target_compile_options(${PLUGIN} PRIVATE 
