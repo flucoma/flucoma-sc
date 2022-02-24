8c2dc69c68e8164716cf015981dfd29cbe8d84d5
weefuzzy <gungwho@gmail.com>
2022-02-24 13:36:25 +0000

    optional args: sc wrapper updates

--- include/wrapper/ArgsFromClient.hpp
+++ include/wrapper/ArgsFromClient.hpp
@@ -135,38 +135,45 @@ struct ParamReader<sc_msg_iter>
     }
   }
   
-  static const char* argTypeToString(std::string&)
+  
+  template <typename T>
+  static const char* argTypeToString(Optional<T>&)
+  {
+    return argTypeToString(T{});
+  }
+  
+  static const char* argTypeToString(const std::string&)
   {
     return "string";
   }
   
   template <typename T>
   static std::enable_if_t<std::is_integral<T>::value, const char*>
-  argTypeToString(T&)
+  argTypeToString(T)
   {
     return "integer";
   }
 
   template <typename T>
   static std::enable_if_t<std::is_floating_point<T>::value, const char*>
-  argTypeToString(T&)
+  argTypeToString(T)
   {
     return "float";
   }
 
-  static const char* argTypeToString(BufferT::type&)
+  static const char* argTypeToString(const BufferT::type&)
   {
     return "buffer";
   }
   
-  static const char* argTypeToString(InputBufferT::type&)
+  static const char* argTypeToString(const InputBufferT::type&)
   {
     return "buffer";
   }
   
   template <typename P>
   static std::enable_if_t<IsSharedClient<P>::value,const char*>
-  argTypeToString(P&)
+  argTypeToString(const P&)
   {
     return "shared_object"; //not ideal
   }
@@ -179,26 +186,32 @@ struct ParamReader<sc_msg_iter>
   template <typename T>
   static std::enable_if_t<std::is_integral<T>::value
                           || std::is_floating_point<T>::value, bool>
-  argTypeOK(T&, char tag)
+  argTypeOK(T, char tag)
   {
     return tag == 'i' || tag == 'f' || tag == 'd';
   }
 
-  static bool argTypeOK(BufferT::type&, char tag)
+  static bool argTypeOK(const BufferT::type&, char tag)
   {
     return tag == 'i';
   }
   
-  static bool argTypeOK(InputBufferT::type&, char tag)
+  static bool argTypeOK(const InputBufferT::type&, char tag)
   {
     return tag == 'i';
   }
   
   template <typename P>
   static std::enable_if_t<IsSharedClient<P>::value,bool>
-  argTypeOK(P&, char tag)
+  argTypeOK(const P&, char tag)
   {
     return tag == 'i';
+  }
+  
+  template<typename T>
+  static bool argTypeOK(const Optional<T>&, char tag)
+  {
+    return argTypeOK(T{},tag);
   }
   
   static auto fromArgs(World*, sc_msg_iter& args, std::string, int)
@@ -266,6 +279,13 @@ struct ParamReader<sc_msg_iter>
         res[i] = static_cast<Value>(args.geti());
       return res;
   }
+  
+  template<typename T>
+  static auto fromArgs(World* w, sc_msg_iter& args, Optional<T>, int)
+  {
+    return Optional<T>{fromArgs(w,args,T{},int{})};
+  }
+  
 };
 
 
