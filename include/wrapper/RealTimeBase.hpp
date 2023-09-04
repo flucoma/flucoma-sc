13454ca646cdd242d33523c95b6718d5b5f55690
tremblap <info@pierrealexandretremblay.com>
2023-09-04 19:37:26 +0200

    pre-clang-format

--- include/wrapper/RealTimeBase.hpp
+++ include/wrapper/RealTimeBase.hpp
@@ -1,63 +1,76 @@
 #pragma once
 
-#include <Eigen/Core>
-#include <SC_PlugIn.hpp>
 #include <data/FluidMemory.hpp>
+#include <SC_PlugIn.hpp>
+#include <Eigen/Core>
 
 namespace fluid {
 namespace client {
 namespace impl {
 
-template <typename Client, class Wrapper> struct RealTimeBase {
-  using IOMapFn = void (RealTimeBase::*)(SCUnit &, Client &);
+
+template <typename Client, class Wrapper>
+struct RealTimeBase
+{
+  using IOMapFn = void (RealTimeBase::*)(SCUnit&, Client&);
   using HostVector = FluidTensorView<float, 1>;
   using Params = typename Client::ParamSetType;
-  template <typename T, bool> struct doExpectedCount;
+  template <typename T, bool>
+  struct doExpectedCount;
 
-  template <typename T> struct doExpectedCount<T, false> {
-    static void count(const T &d, FloatControlsIter &c, Result &status) {
-      if (!status.ok())
-        return;
+  template <typename T>
+  struct doExpectedCount<T, false>
+  {
+    static void count(const T& d, FloatControlsIter& c, Result& status)
+    {
+      if (!status.ok()) return;
 
-      if (c.remain()) {
+      if (c.remain())
+      {
         index statedSize = d.fixedSize;
 
         if (c.remain() < statedSize)
           status = {Result::Status::kError, "Ran out of arguments at ", d.name};
 
         // fastforward
-        for (index i = 0; i < statedSize; ++i)
-          c.next();
+        for (index i = 0; i < statedSize; ++i) c.next();
       }
     }
   };
 
-  template <typename T> struct doExpectedCount<T, true> {
-    static void count(const T &d, FloatControlsIter &c, Result &status) {
-      if (!status.ok())
-        return;
+  template <typename T>
+  struct doExpectedCount<T, true>
+  {
+    static void count(const T& d, FloatControlsIter& c, Result& status)
+    {
+      if (!status.ok()) return;
 
-      if (c.remain()) {
+      if (c.remain())
+      {
         index statedSize = 1;
 
         if (c.remain() < statedSize)
           status = {Result::Status::kError, "Ran out of arguments at ", d.name};
 
         // fastforward
-        for (index i = 0; i < statedSize; ++i)
-          c.next();
+        for (index i = 0; i < statedSize; ++i) c.next();
       }
     }
   };
-  template <size_t N, typename T> struct ExpectedCount {
-    void operator()(const T &descriptor, FloatControlsIter &c, Result &status) {
+  template <size_t N, typename T>
+  struct ExpectedCount
+  {
+    void operator()(const T& descriptor, FloatControlsIter& c, Result& status)
+    {
       doExpectedCount<T, IsSharedClientRef<typename T::type>::value>::count(
           descriptor, c, status);
     }
   };
 
-  Result expectedSize(FloatControlsIter &controls) {
-    if (controls.size() < Client::getParameterDescriptors().count()) {
+  Result expectedSize(FloatControlsIter& controls)
+  {
+    if (controls.size() < Client::getParameterDescriptors().count())
+    {
       return {Result::Status::kError, "Fewer parameters than exepected. Got ",
               controls.size(), "expect at least",
               Client::getParameterDescriptors().count()};
@@ -65,22 +78,24 @@ template <typename Client, class Wrapper> struct RealTimeBase {
 
     Result countScan;
     Client::getParameterDescriptors().template iterate<ExpectedCount>(
-        std::forward<FloatControlsIter &>(controls),
-        std::forward<Result &>(countScan));
+        std::forward<FloatControlsIter&>(controls),
+        std::forward<Result&>(countScan));
     return countScan;
   }
+    
 
-  void init(SCUnit &unit, Client &client, FloatControlsIter &controls,
-            Allocator &alloc) {
+  void init(SCUnit& unit, Client& client, FloatControlsIter& controls, Allocator& alloc)
+  {
     assert(!(client.audioChannelsOut() > 0 &&
              client.controlChannelsOut().count > 0) &&
            "Client can't have both audio and control outputs");
     client.sampleRate(unit.fullSampleRate());
     mInputConnections.reserve(asUnsigned(client.audioChannelsIn()));
     mOutputConnections.reserve(asUnsigned(client.audioChannelsOut()));
-    mContext = FluidContext(unit.fullBufferSize(), alloc);
+    mContext = FluidContext(unit.fullBufferSize(), alloc); 
     Result r;
-    if (!(r = expectedSize(controls)).ok()) {
+    if (!(r = expectedSize(controls)).ok())
+    {
       std::cout << "ERROR: " << Wrapper::getName()
                 << " wrong number of arguments." << r.message() << std::endl;
       return;
@@ -102,29 +117,34 @@ template <typename Client, class Wrapper> struct RealTimeBase {
       }
       mInputMapper = &RealTimeBase::mapControlInputs;
     } else {
-      mInputMapper = &RealTimeBase::mapNoOp;
+        mInputMapper = &RealTimeBase::mapNoOp;
     }
-
+      
     index outputSize = client.controlChannelsOut().size > 0
                            ? std::max(client.audioChannelsOut(),
                                       client.maxControlChannelsOut())
-                           : (unit.mSpecialIndex + 1);
-
+      : (unit.mSpecialIndex + 1);
+      
     mOutputs.reserve(asUnsigned(outputSize));
 
-    if (client.audioChannelsOut()) {
-      for (index i = 0; i < client.audioChannelsOut(); ++i) {
+    if (client.audioChannelsOut())
+    {
+      for (index i = 0; i < client.audioChannelsOut(); ++i)
+      {
         mOutputConnections.emplace_back(true);
         mOutputs.emplace_back(nullptr, 0, 0);
       }
-
+      
       mOutMapperPre = &RealTimeBase::mapAudioOutputs;
       mOutMapperPost = &RealTimeBase::mapNoOp;
-    } else {
+    }
+    else
+    {
       index totalControlOutputs =
           client.controlChannelsOut().count * outputSize;
       mControlOutputBuffer.resize(totalControlOutputs);
-      for (index i = 0; i < client.controlChannelsOut().count; ++i) {
+      for (index i = 0; i < client.controlChannelsOut().count; ++i)
+      {
         mOutputs.emplace_back(
             mControlOutputBuffer(fluid::Slice(i * outputSize, outputSize)));
       }
@@ -134,20 +154,24 @@ template <typename Client, class Wrapper> struct RealTimeBase {
     }
   }
 
-  void mapNoOp(SCUnit &, Client &) {}
+  void mapNoOp(SCUnit&, Client&) {}
 
-  void mapAudioInputs(SCUnit &unit, Client &client) {
-    for (index i = 0; i < client.audioChannelsIn(); ++i) {
+  void mapAudioInputs(SCUnit& unit, Client& client)
+  {
+    for (index i = 0; i < client.audioChannelsIn(); ++i)
+    {
       assert(i <= std::numeric_limits<int>::max());
       if (mInputConnections[asUnsigned(i)])
         mAudioInputs[asUnsigned(i)].reset(
-            const_cast<float *>(unit.in(static_cast<int>(i))), 0,
+            const_cast<float*>(unit.in(static_cast<int>(i))), 0,
             unit.fullBufferSize());
     }
   }
 
-  void mapAudioOutputs(SCUnit &unit, Client &client) {
-    for (index i = 0; i < client.audioChannelsOut(); ++i) {
+  void mapAudioOutputs(SCUnit& unit, Client& client)
+  {
+    for (index i = 0; i < client.audioChannelsOut(); ++i)
+    {
       assert(i <= std::numeric_limits<int>::max());
       if (mOutputConnections[asUnsigned(i)])
         mOutputs[asUnsigned(i)].reset(unit.out(static_cast<int>(i)), 0,
@@ -169,27 +193,30 @@ template <typename Client, class Wrapper> struct RealTimeBase {
     }
   }
 
-  void mapControlOutputs(SCUnit &unit, Client &) {
-    index numOuts =
-        std::min<index>(mControlOutputBuffer.size(), unit.mNumOutputs);
-
-    for (index i = 0; i < numOuts; ++i) {
+  void mapControlOutputs(SCUnit& unit, Client&)
+  {
+    index numOuts = std::min<index>(mControlOutputBuffer.size(),unit.mNumOutputs);
+    
+    for (index i = 0; i < numOuts; ++i)
+    {
       assert(i <= std::numeric_limits<int>::max());
       unit.out0(static_cast<int>(i)) = mControlOutputBuffer(i);
     }
   }
 
-  void next(SCUnit &unit, Client &client, Params &params,
-            FloatControlsIter &controls, Allocator &alloc,
-            bool updateParams = true) {
+  void next(SCUnit& unit, Client& client, Params& params,
+            FloatControlsIter& controls, Allocator& alloc,
+            bool updateParams = true)
+  {
     bool trig =
         IsModel_t<Client>::value ? !mPrevTrig && unit.in0(0) > 0 : false;
 
     mPrevTrig = trig;
-#ifdef EIGEN_RUNTIME_NO_MALLOC
+    #ifdef EIGEN_RUNTIME_NO_MALLOC
     Eigen::internal::set_is_malloc_allowed(false);
-#endif
-    if (updateParams) {
+    #endif
+    if (updateParams)
+    {
       Wrapper::setParams(&unit, params, controls, alloc);
       params.constrainParameterValuesRT(nullptr);
     }
@@ -198,23 +225,23 @@ template <typename Client, class Wrapper> struct RealTimeBase {
     (this->*mOutMapperPre)(unit, client);
     client.process(mAudioInputs, mOutputs, mContext);
     (this->*mOutMapperPost)(unit, client);
-#ifdef EIGEN_RUNTIME_NO_MALLOC
-    Eigen::internal::set_is_malloc_allowed(true); // not really
-#endif
+    #ifdef EIGEN_RUNTIME_NO_MALLOC
+    Eigen::internal::set_is_malloc_allowed(true); //not really
+    #endif
   }
 
 private:
-  std::vector<bool> mInputConnections;
-  std::vector<bool> mOutputConnections;
+  std::vector<bool>       mInputConnections;
+  std::vector<bool>       mOutputConnections;
   std::vector<HostVector> mAudioInputs;
   std::vector<HostVector> mOutputs;
-  FluidTensor<float, 2> mControlInputBuffer;
-  FluidTensor<float, 1> mControlOutputBuffer;
-  bool mPrevTrig;
-  IOMapFn mInputMapper;
-  IOMapFn mOutMapperPre;
-  IOMapFn mOutMapperPost;
-  FluidContext mContext;
+  FluidTensor<float, 2>   mControlInputBuffer;
+  FluidTensor<float, 1>   mControlOutputBuffer;
+  bool                    mPrevTrig;
+  IOMapFn                 mInputMapper;
+  IOMapFn                 mOutMapperPre;
+  IOMapFn                 mOutMapperPost;
+  FluidContext            mContext;
 };
 } // namespace impl
 } // namespace client
