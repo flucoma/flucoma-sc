4c5a06fc6ea7cfb9ff1cfeed24f361691cdd6c38
tremblap <info@pierrealexandretremblay.com>
2023-09-04 22:52:12 +0200

    clang-format-ed with the right style sheet

--- include/wrapper/RealTimeBase.hpp
+++ include/wrapper/RealTimeBase.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <Eigen/Core>
 #include <data/FluidMemory.hpp>
 #include <SC_PlugIn.hpp>
-#include <Eigen/Core>
 
 namespace fluid {
 namespace client {
@@ -82,9 +82,10 @@ struct RealTimeBase
         std::forward<Result&>(countScan));
     return countScan;
   }
-    
 
-  void init(SCUnit& unit, Client& client, FloatControlsIter& controls, Allocator& alloc)
+
+  void init(SCUnit& unit, Client& client, FloatControlsIter& controls,
+            Allocator& alloc)
   {
     assert(!(client.audioChannelsOut() > 0 &&
              client.controlChannelsOut().count > 0) &&
@@ -92,7 +93,7 @@ struct RealTimeBase
     client.sampleRate(unit.fullSampleRate());
     mInputConnections.reserve(asUnsigned(client.audioChannelsIn()));
     mOutputConnections.reserve(asUnsigned(client.audioChannelsOut()));
-    mContext = FluidContext(unit.fullBufferSize(), alloc); 
+    mContext = FluidContext(unit.fullBufferSize(), alloc);
     Result r;
     if (!(r = expectedSize(controls)).ok())
     {
@@ -101,30 +102,34 @@ struct RealTimeBase
       return;
     }
 
-    if (client.audioChannelsIn()) {
+    if (client.audioChannelsIn())
+    {
       mAudioInputs.reserve(asUnsigned(client.audioChannelsIn()));
-      for (index i = 0; i < client.audioChannelsIn(); ++i) {
+      for (index i = 0; i < client.audioChannelsIn(); ++i)
+      {
         mInputConnections.emplace_back(unit.isAudioRateIn(static_cast<int>(i)));
         mAudioInputs.emplace_back(nullptr, 0, 0);
       }
       mInputMapper = &RealTimeBase::mapAudioInputs;
-    } else if (client.controlChannelsIn()) {
+    }
+    else if (client.controlChannelsIn())
+    {
       mControlInputBuffer.resize(client.controlChannelsIn(),
                                  (unit.mSpecialIndex + 1) /
                                      client.controlChannelsIn());
-      for (index i = 0; i < client.controlChannelsIn(); ++i) {
+      for (index i = 0; i < client.controlChannelsIn(); ++i)
+      {
         mAudioInputs.emplace_back(mControlInputBuffer.row(i));
       }
       mInputMapper = &RealTimeBase::mapControlInputs;
-    } else {
-        mInputMapper = &RealTimeBase::mapNoOp;
     }
-      
+    else { mInputMapper = &RealTimeBase::mapNoOp; }
+
     index outputSize = client.controlChannelsOut().size > 0
                            ? std::max(client.audioChannelsOut(),
                                       client.maxControlChannelsOut())
-      : (unit.mSpecialIndex + 1);
-      
+                           : (unit.mSpecialIndex + 1);
+
     mOutputs.reserve(asUnsigned(outputSize));
 
     if (client.audioChannelsOut())
@@ -134,7 +139,7 @@ struct RealTimeBase
         mOutputConnections.emplace_back(true);
         mOutputs.emplace_back(nullptr, 0, 0);
       }
-      
+
       mOutMapperPre = &RealTimeBase::mapAudioOutputs;
       mOutMapperPost = &RealTimeBase::mapNoOp;
     }
@@ -179,14 +184,17 @@ struct RealTimeBase
     }
   }
 
-  void mapControlInputs(SCUnit &unit, Client &client) {
+  void mapControlInputs(SCUnit& unit, Client& client)
+  {
     assert((unit.mSpecialIndex + 1) % client.controlChannelsIn() == 0 &&
            "Control channels can't be mapped");
     index itemsPerChannel =
         (unit.mSpecialIndex + 1) / client.controlChannelsIn();
     for (index i = 0, offset = 0; i < client.controlChannelsIn();
-         ++i, offset += itemsPerChannel) {
-      for (index j = 0; j < itemsPerChannel; ++j) {
+         ++i, offset += itemsPerChannel)
+    {
+      for (index j = 0; j < itemsPerChannel; ++j)
+      {
         assert(j <= std::numeric_limits<int>::max());
         mControlInputBuffer(i, j) = unit.in0(static_cast<int>(offset + j));
       }
@@ -195,8 +203,9 @@ struct RealTimeBase
 
   void mapControlOutputs(SCUnit& unit, Client&)
   {
-    index numOuts = std::min<index>(mControlOutputBuffer.size(),unit.mNumOutputs);
-    
+    index numOuts =
+        std::min<index>(mControlOutputBuffer.size(), unit.mNumOutputs);
+
     for (index i = 0; i < numOuts; ++i)
     {
       assert(i <= std::numeric_limits<int>::max());
@@ -212,9 +221,9 @@ struct RealTimeBase
         IsModel_t<Client>::value ? !mPrevTrig && unit.in0(0) > 0 : false;
 
     mPrevTrig = trig;
-    #ifdef EIGEN_RUNTIME_NO_MALLOC
+#ifdef EIGEN_RUNTIME_NO_MALLOC
     Eigen::internal::set_is_malloc_allowed(false);
-    #endif
+#endif
     if (updateParams)
     {
       Wrapper::setParams(&unit, params, controls, alloc);
@@ -225,9 +234,9 @@ struct RealTimeBase
     (this->*mOutMapperPre)(unit, client);
     client.process(mAudioInputs, mOutputs, mContext);
     (this->*mOutMapperPost)(unit, client);
-    #ifdef EIGEN_RUNTIME_NO_MALLOC
-    Eigen::internal::set_is_malloc_allowed(true); //not really
-    #endif
+#ifdef EIGEN_RUNTIME_NO_MALLOC
+    Eigen::internal::set_is_malloc_allowed(true); // not really
+#endif
   }
 
 private:
